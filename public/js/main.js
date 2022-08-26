23e5df1281dd58746bcb0db08d1ede0427c0175c
lst4rk <olivialistokin@gmail.com>
2022-08-26 14:14:25 -0400

    styled form and posts

--- public/js/main.js
+++ public/js/main.js
@@ -2,18 +2,18 @@ const deleteText = document.querySelectorAll('.fa-trash')
 const thumbText = document.querySelectorAll('.fa-thumbs-up')
 
 Array.from(deleteText).forEach((element)=>{
-    element.addEventListener('click', deleteRapper)
+    element.addEventListener('click', deleteThought)
 })
 
 Array.from(thumbText).forEach((element)=>{
     element.addEventListener('click', addLike)
 })
 
-async function deleteRapper(){
+async function deleteThought(){
     const sName = this.parentNode.childNodes[1].innerText
     const bName = this.parentNode.childNodes[3].innerText
     try{
-        const response = await fetch('deleteRapper', {
+        const response = await fetch('deleteThought', {
             method: 'delete',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
--- server.js
+++ server.js
@@ -7,7 +7,7 @@ require('dotenv').config()
 
 let db,
     dbConnectionStr = process.env.DB_STRING,
-    dbName = 'rap'
+    dbName = 'thought'
 
 MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
     .then(client => {
@@ -22,45 +22,46 @@ app.use(express.json())
 
 
 app.get('/',(request, response)=>{
-    db.collection('rappers').find().sort({likes: -1}).toArray()
+    db.collection('thinkingthings').find().sort({likes: -1}).toArray()
     .then(data => {
         response.render('index.ejs', { info: data })
     })
     .catch(error => console.error(error))
 })
 
-app.post('/addRapper', (request, response) => {
-    db.collection('rappers').insertOne({stageName: request.body.stageName,
-    birthName: request.body.birthName, likes: 0})
+app.post('/addThought', (request, response) => {
+    db.collection('thinkingthings').insertOne({thought: request.body.thought,
+    signoff: request.body.signoff, likes: 0})
     .then(result => {
-        console.log('Rapper Added')
+        console.log('thought acknowledged')
         response.redirect('/')
     })
     .catch(error => console.error(error))
 })
 
 app.put('/addOneLike', (request, response) => {
-    db.collection('rappers').updateOne({stageName: request.body.stageNameS, birthName: request.body.birthNameS,likes: request.body.likesS},{
+    db.collection('thinkingthings').updateOne({thought: request.body.stageNameS, signoff: request.body.birthNameS,likes: request.body.likesS},{
         $set: {
             likes:request.body.likesS + 1
           }
     },{
         sort: {_id: -1},
-        upsert: true
+        upsert: false
     })
     .then(result => {
-        console.log('Added One Like')
-        response.json('Like Added')
+        console.log('emphathized')
+        response.json('empathized')
     })
     .catch(error => console.error(error))
 
 })
 
-app.delete('/deleteRapper', (request, response) => {
-    db.collection('rappers').deleteOne({stageName: request.body.stageNameS})
+app.delete('/deleteThought', (request, response) => {
+    db.collection('thinkingthings').deleteOne({thought: request.body.stageNameS})
     .then(result => {
-        console.log('Rapper Deleted')
-        response.json('Rapper Deleted')
+        console.log('thought taken back')
+        response.json('thought taken back')
+        response.redirect('/')
     })
     .catch(error => console.error(error))
 
