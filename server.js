cbbdd7a1e71a55b47dacb3696c30df2850d7ee2e
lst4rk <olivialistokin@gmail.com>
2022-08-28 11:02:01 -0400

    fixed variable

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const MongoClient = require('mongodb').MongoClient
 const PORT = 2121
+const cors = require('cors');
 require('dotenv').config()
 
 
